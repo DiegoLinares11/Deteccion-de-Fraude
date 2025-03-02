f46b179a7360da92091720e2ae37cc917a5f5afa
DiegoLinares11 <lin221256@uvg.edu.gt>
2025-03-01 20:34:29 -0600

    add deviceRoutes

---
+++ backend/src/routes/deviceRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const deviceController = require('../controllers/deviceController');
+const router = express.Router();
+
+router.post('/', deviceController.createDevice);
+router.get('/', deviceController.getAllDevices);
+router.get('/:id', deviceController.getDeviceById);
+router.put('/:id', deviceController.updateDevice);
+router.delete('/:id', deviceController.deleteDevice);
+
+module.exports = router;
